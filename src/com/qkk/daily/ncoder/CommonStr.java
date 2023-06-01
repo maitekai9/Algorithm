c29e8e512c897a5cb5a99bea796a83bff95fcd54
maitekai <416911003@qq.com>
2023-06-01 20:21:43 +0800

    ignore

--- src/com/qkk/daily/ncoder/CommonStr.java
+++ src/com/qkk/daily/ncoder/CommonStr.java
@@ -11,16 +11,16 @@ public class CommonStr {
     public String getCommon(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n + 1][m + 1];
         int max = 0;
         int index = 0;
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
-                if (s1.charAt(i) == s2.charAt(j)) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                     if (dp[i][j] > max) {
                         max = dp[i][j];
-                        index = i;
+                        index = i - max;
                     }
                 }
             }
